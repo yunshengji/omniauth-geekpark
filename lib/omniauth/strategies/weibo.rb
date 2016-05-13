e6d41bf46895b82a3917ca220a8040231c74521b
Gavin Li <gavin.li1986@gmail.com>
2016-05-13 13:14:06 +0800

    Remove unnecessary codes

--- lib/omniauth/strategies/weibo.rb
+++ lib/omniauth/strategies/weibo.rb
@@ -5,50 +5,30 @@ module OmniAuth
         site: "https://api.weibo.com",
         authorize_url: "/oauth2/authorize",
         token_url: "/oauth2/access_token",
-        token_method: :post
-      }
-      option :token_params, {
-        :parse          => :json
       }
 
-      uid do
-        raw_info['id']
-      end
+      option :token_params, parse: :json
+
+      uid { raw_info['id'] }
 
       info do
         {
-          :nickname     => raw_info['screen_name'],
-          :avatar       => raw_info['avatar_large'],
-          :name         => raw_info['name'],
-          :location     => raw_info['location'],
-          :description  => raw_info['description'],
+          nickname: raw_info['screen_name'],
+          avatar: raw_info['avatar_large'],
+          bio: raw_info['description']
         }
       end
 
-      extra do
-        {
-          :raw_info => raw_info
-        }
-      end
+      extra do { raw_info: raw_info } end
 
       def raw_info
-        access_token.options[:mode] = :query
-        access_token.options[:param_name] = 'access_token'
-        @uid ||= access_token.get('/2/account/get_uid.json').parsed["uid"]
-        @raw_info ||= access_token.get("/2/users/show.json", :params => {:uid => @uid}).parsed
-      end
+        @uid ||= begin
+                   access_token.options[:mode] = :query
+                   access_token.get('/2/account/get_uid.json').parsed["uid"]
+                 end
 
-      protected
-      def build_access_token
-        params = {
-          'client_id' => client.id,
-          'client_secret' => client.secret,
-          'code' => request.params['code'],
-          'grant_type' => 'authorization_code'
-        }.merge(token_params.to_hash(symbolize_keys: true))
-        client.get_token(params, deep_symbolize(options.auth_token_params))
+        @raw_info ||= access_token.get('/2/users/show.json', params: {uid: @uid}).parsed
       end
-
     end
   end
 end
