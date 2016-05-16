e7308e4415f0ae02c342e4cf6beeed7ac00c5001
Gavin Li <gavin.li1986@gmail.com>
2016-05-16 15:48:12 +0800

    Change geekpark strategy

--- lib/omniauth/strategies/geekpark.rb
+++ lib/omniauth/strategies/geekpark.rb
@@ -2,29 +2,26 @@ module OmniAuth
   module Strategies
     class GeekPark < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        site: 'http://www.geekpark.net',
-        authorize_url: 'http://www.geekpark.net/oauth/authorize',
-        token_url: 'http://www.geekpark.net/oauth/token'
+        site: 'https://account.geekpark.net',
+        authorize_url: '/oauth2/authorize',
+        token_url: '/oauth2/token'
       }
 
-      uid { raw_info['uuid'] }
+      uid { raw_info['id'] }
 
       info do
         {
-          username: raw_info['username'],
-          realname: raw_info['realname'],
-          email: raw_info['email'],
-          company: raw_info['company'],
-          position: raw_info['position'],
-          mobile: raw_info['mobile'],
-          avatar: raw_info['avatar'],
+          nickname: raw_info['nickname'],
+          gender: raw_info['gender'],
+          city: raw_info['city'],
           bio: raw_info['bio'],
-          mygeekpark_open_id: raw_info['mygeekpark_open_id'],
+          position: raw_info['position'],
+          avatar_url: raw_info['avatar_url']
         }
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/api/v1/user').parsed || {}
+        @raw_info ||= access_token.get('/api/v1/user').parsed
       end
     end
   end
